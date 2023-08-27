4376b895a2848f74bc9ee08fe8fe281c1bb1e354
Aymen Idouahman <aymanidou@gmail.com>
2023-08-27 22:11:57 +0100

    task 7.div

--- monty.h
+++ monty.h
@@ -87,6 +87,7 @@ void _swap(stack_t **doubly, unsigned int cline);
 void _add(stack_t **doubly, unsigned int cline);
 void _nop(stack_t **doubly, unsigned int cline);
 void _sub(stack_t **doubly, unsigned int cline);
+void _div(stack_t **doubly, unsigned int cline);
 
 
 /* String functions*/
